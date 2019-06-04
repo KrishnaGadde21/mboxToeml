87242d3835b0db0972a576dec8d1bd4bbeb497b2
Krishna Gadde <45312900+KrishnaGadde21@users.noreply.github.com>
2019-06-04 16:46:59 +0530

    Changed function for SAVEEML and added extractattch
    Changed the fuction to create .eml files as MIMEType should also be taken into consideration, which was  not possible in the Last commit.
    Also added a new function to extract attachments from the given .mbox file

--- mboxToeml.py
+++ mboxToeml.py
@@ -1,9 +1,48 @@
+import mailbox,os
+from email import generator
+#from email.header import decode_header
+#import base64
 
-import mailbox
 
 mbx = mailbox.mbox("sample.mbox")
 
+saveEML_to = 'MboxToEmlAndAttch/EML/'
+if not os.path.exists(saveEML_to):
+    os.makedirs(saveEML_to)
+
+
+saveAttch_to = 'MboxToEmlAndAttch/Attch/'
+if not os.path.exists(saveAttch_to):
+    os.makedirs(saveAttch_to)
+
 for k,v in mbx.iteritems():
-    f=open("test"+str(k)+".eml",'w')
-    f.write(str(v))
-           
+    with open(saveEML_to+str(k)+".eml",'w') as f:
+        gen = generator.Generator(f)
+        gen.flatten(v)
+        
+        
+
+def extractattch(i):
+    if i.get_content_maintype()=='multipart':
+        for part in i.walk():
+            if part.get_content_maintype() == 'multipart':
+                continue
+            if part.get('Content-Disposition') is None:
+                continue
+
+            f=part.get_filename()
+            print(f)
+
+
+            if not os.path.exists(saveAttch_to):
+                os.makedirs(saveAttch_to)
+
+
+            fb = open( saveAttch_to+f,'wb')
+            fb.write(part.get_payload(decode=True))
+            fb.close()
+
+            print()
+
+for i in mbx:
+    extractattch(i)        
